173dd88f609fc61e65940421c1b89ce6bc12014d
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-04-19 13:43:34 +0100

    map api displaying current coords with marker

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// prettier-ignore
+
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
@@ -10,3 +10,47 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+
+    function (position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(`https://www.google.co.uk/maps/@${latitude},${longitude}`);
+
+        const coords = [latitude, longitude];
+
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        // const marker = L.marker(coords).addTo(map);
+
+        map.on("click", function (mapEvent) {
+
+            form.classList.remove("hidden");
+            inputDistance.focus();
+
+            const { lat, lng } = mapEvent.latlng;
+
+            L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: "running-popup"
+            })).setPopupContent("Workout").openPopup();
+        });
+    },
+
+    function () {
+        alert("Could not determine your position!")
+    }
+);
+
+
+
+
